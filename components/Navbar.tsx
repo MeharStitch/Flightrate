585d2242003ef76f0bf11927dd6e5e4311f6b734
MeharStitch <Zeeshansajid.meet@gmail.com>
2026-05-10 10:01:09 +0500

    fix: logo links to home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
 
       <div className="navbar-wrap">
         <nav className={`navbar ${scrolled ? 'navbar-scrolled' : ''}`}>
-          <a href="#" className="nav-logo">
+          <a href="/" className="nav-logo">
             <div className="nav-logo-icon">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
                 <path d="M21 16v-2l-8-5V3.5a1.5 1.5 0 0 0-3 0V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z" fill="white"/>
